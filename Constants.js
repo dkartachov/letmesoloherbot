a2b6ff67e166342334cf96dcd1bec40c4bd5b2af
Denis Kartachov <kartachovd@gmail.com>
2022-04-28 20:19:27 -0400

    add Constants.js

---
+++ Constants.js
@@ -0,0 +1,8 @@
+export const replies = [
+    'Summon me.',
+    'Let me solo her.',
+    'Leave her to me.',
+    'Fellow Jarnished at your service.',
+    'Tarnished at your service.',
+    'I will lend you my blade.',
+];
